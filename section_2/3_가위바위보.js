975eb3662a8e760d17afe655f5425921b45afbd0
JunHee Lee <jhlee910609@gmail.com>
2021-08-30 08:37:56 +0900

    August 30, 2021 8:37 AM

---
+++ section_2/3_가위바위보.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+  let answer = "";
+  for (let i = 0; i < a.length; i++) {
+      if(a)
+  }
+
+  return answer;
+}
+
+let a = [2, 3, 3, 1, 3];
+let b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
