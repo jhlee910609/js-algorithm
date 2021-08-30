f75b6c7384bc97c9c5830866a756956bd93083c8
JunHee Lee <jhlee910609@gmail.com>
2021-08-31 01:22:37 +0900

    August 31, 2021 1:22 AM

--- section_2/5_등수구하기.js
+++ section_2/5_등수구하기.js
@@ -1,6 +1,12 @@
 // 나포함 앞에 몇 명이 있냐
 function solution(arr) {
-  let n = arr.length;
+  const answer = [];
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i; j < len; j++) {
+      console.log(arr[i], arr[j]);
+    }
+  }
   return answer;
 }
 
