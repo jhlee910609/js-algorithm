a85802a17ff888a5455135743834d5db3726e125
JunHee Lee <jhlee910609@gmail.com>
2021-09-02 06:59:30 +0900

    September 2, 2021 6:59 AM

--- section_2/7_봉우리.js
+++ section_2/7_봉우리.js
@@ -7,7 +7,10 @@ function solution(arr) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       for (let k = 0; k < 4; k++) {
-        console.log(dx[k], dy[k]);
+        const nx = i + dx[k];
+        const ny = j + dy[k];
+        if (arr[i][j]) {
+        }
       }
     }
   }
