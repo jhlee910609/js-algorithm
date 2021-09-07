9288c96b4c520e5c17200766ddb85d859184c7c2
JunHee Lee <jhlee910609@gmail.com>
2021-09-08 01:07:21 +0900

    September 8, 2021 1:07 AM

--- section_3/1_회문.js
+++ section_3/1_회문.js
@@ -1,14 +1,11 @@
 function solution(word) {
   const len = word.length;
-  if (len % 2 === 0) {
-    for (let i = 0; i < len / 2; i++) {
-      if (word[i].toLowerCase() !== word[len - i - 1].toLowerCase()) {
-        return "NO";
-      }
-      return "YES";
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i].toLowerCase() !== word[len - i - 1].toLowerCase()) {
+      return "NO";
     }
-  } else {
+    return "YES";
   }
 }
 
-console.log(solution("gooG"));
+console.log(solution("goo"));
