4461da4005a5cd3985a0c2615b16b62683fc319f
JunHee Lee <jhlee910609@gmail.com>
2021-09-08 06:53:03 +0900

    September 8, 2021 6:53 AM

---
+++ section_3/2_유효한_회문.js
@@ -0,0 +1,9 @@
+function solution(input) {
+  let answer = "YES";
+  input = input.replace("/[^a-zA-Z]/g", "");
+  console.log(input);
+
+  return answer;
+}
+
+console.log(solution("found7, time: study; Yduts; emit, 7Dnuof"));
