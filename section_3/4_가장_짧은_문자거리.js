de67703c6d7b8f2e23781076075ed61ed1883980
JunHee Lee <jhlee910609@gmail.com>
2021-09-09 00:31:07 +0900

    September 9, 2021 12:31 AM

---
+++ section_3/4_가장_짧은_문자거리.js
@@ -0,0 +1,9 @@
+function solution(input, target) {
+  const len = input.length;
+  let answer = "";
+  for (let i = 0; i < len; i++) {
+    if (input[i] === target) answer += "0";
+  }
+}
+
+console.log(solution("teachermode", "e"));
