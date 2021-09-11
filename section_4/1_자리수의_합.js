b887d0442f185255b43dc5dea5956b89c8a5b305
JunHee Lee <jhlee910609@gmail.com>
2021-09-11 13:58:41 +0900

    September 11, 2021 1:58 PM

--- section_4/1_자리수의_합.js
+++ section_4/1_자리수의_합.js
@@ -1,6 +1,15 @@
-function soultion(nums) {
-  let answer;
-  nums.split(" ").forEach((item) => {});
+function solution(input) {
+  let answer = -1;
+  const numbs = input.split(" ");
+  for (let i = 0; i < numbs.length; i++) {
+    let temp = numbs[i];
+    let sum = 0;
+    while (temp !== 0) {
+      sum += temp % 10;
+      temp = Math.floor(temp / 10);
+    }
+    answer = Math.max(answer, sum);
+  }
 
   return answer;
 }
