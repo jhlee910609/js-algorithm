f3fb210883e0d9da731c48b550c6f8ba3177fd93
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 08:19:21 +0900

    September 17, 2021 8:19 AM

---
+++ section_4/2_뒤집은_소수.js
@@ -0,0 +1,10 @@
+function solution(input) {
+  const answer = [];
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+  }
+  return answer;
+}
+
+const inputs = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solution(inputs));
