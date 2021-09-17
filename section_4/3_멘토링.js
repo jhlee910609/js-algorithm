312b5c2e2e9e5b95c2d3e6273200b4ee4dc44ea4
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 20:09:25 +0900

    September 17, 2021 8:09 PM

--- section_4/3_멘토링.js
+++ section_4/3_멘토링.js
@@ -6,21 +6,27 @@ function solution(inputArray) {
   for (let i = 1; i <= N; i++) {
     for (let j = 1; j <= N; j++) {
       let cnt = 0;
+      // input 모두 순회
       for (let k = 0; k < M; k++) {
+        // 순회하면서 멘토, 멘티 등수
         let pi = (pj = 0);
         for (let l = 0; l < N; l++) {
+          // 멘토 등수
           if (inputArray[k][l] === i) {
             pi = l;
           }
 
+          // 멘티 등수
           if (inputArray[k][l] === j) {
             pj = l;
           }
         }
+        // 멘토 > 멘티 순서 cnt++
         if (pi < pj) {
           cnt++;
         }
       }
+      // 모든 시험에서 멘토 > 멘티 answer++
       if (cnt === M) answer++;
     }
   }
