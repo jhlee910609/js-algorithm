97f43e83605f9802508e1f1afda75b1d9c54ba38
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 19:51:34 +0900

    September 17, 2021 7:51 PM

--- section_4/3_멘토링.js
+++ section_4/3_멘토링.js
@@ -1,29 +1,11 @@
-function solution(test) {
+function solution(inputArray) {
   let answer = 0;
-  m = test.length;
-  n = test[0].length;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      let cnt = 0;
-      for (let k = 0; k < m; k++) {
-        let pi = (pj = 0);
-        for (let s = 0; s < n; s++) {
-          console.log(test[k][s]);
-          if (test[k][s] === i) pi = s;
-          if (test[k][s] === j) pj = s;
-        }
-        console.log(pi, pj);
-        if (pi < pj) cnt++;
-      }
-      if (cnt === m) answer++;
-    }
-  }
   return answer;
 }
 
-let arr = [
+const testResult = [
   [3, 4, 1, 2],
   [4, 3, 2, 1],
   [3, 1, 4, 2],
 ];
-console.log(solution(arr));
+console.log(solution(testResult));
