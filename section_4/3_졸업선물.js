918448873d6c197b48277328f0b2bcca3b815357
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 22:16:47 +0900

    September 17, 2021 10:16 PM

--- section_4/3_졸업선물.js
+++ section_4/3_졸업선물.js
@@ -1,7 +1,26 @@
 // 뭔가를 구하려고 하는데 기준이 명확하지 않다? 모든 조건에 대해 다 찾아야 한다. > brute force
-function solution(input) {
+function solution(input, budget) {
   let answer = 0;
-  // 1,
+  // 1.
+  input = input.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      console.log(input[i][j]);
+    }
+  }
 
   return answer;
 }
+
+console.log(
+  solution(
+    [
+      [6, 6],
+      [2, 2],
+      [4, 3],
+      [4, 5],
+      [10, 3],
+    ],
+    28
+  )
+);
