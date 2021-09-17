35d34ef7f71fa9563bf1a47310c577e8ba099578
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 20:01:19 +0900

    September 17, 2021 8:01 PM

--- section_4/3_멘토링.js
+++ section_4/3_멘토링.js
@@ -2,7 +2,29 @@ function solution(inputArray) {
   let answer = 0;
   const M = inputArray.length;
   const N = inputArray[0].length;
-  console.log(M, N);
+  // i = 멘토, j = 멘티
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      let cnt = 0;
+      for (let k = 0; k < M; k++) {
+        for (let l = 0; l < N; l++) {
+          let pi = (pj = 0);
+          if (inputArray[k][l] === i) {
+            pi = l;
+          }
+
+          if (inputArray[k][l] === j) {
+            pj = l;
+          }
+          if (pi < pj) {
+            console.log(pi, pj);
+            cnt++;
+          }
+        }
+        if (cnt === M) answer++;
+      }
+    }
+  }
   return answer;
 }
 
