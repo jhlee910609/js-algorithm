1fc799f362d48045f57f6fef560c76fe73372489
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 22:59:01 +0900

    September 17, 2021 10:59 PM

--- section_4/5_k번째_큰_수.js
+++ section_4/5_k번째_큰_수.js
@@ -1,7 +1,9 @@
 function solution(cards, k) {
   let answer;
-  if (k === 1) return answer;
-  return answer;
+
+  cards.forEach((card, i, origin) => {
+    console.log(card, i, origin);
+  });
 }
 
 console.log(solution([13, 15, 34, 23, 45, 65, 33, 11, 26, 42], 3));
