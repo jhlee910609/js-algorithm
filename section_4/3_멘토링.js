00a7767978b36284de505667a87a4dc8fa8afbc7
JunHee Lee <jhlee910609@gmail.com>
2021-09-17 19:16:35 +0900

    September 17, 2021 7:16 PM

---
+++ section_4/3_멘토링.js
@@ -0,0 +1,14 @@
+function solution(input) {
+  let answer = [];
+  for (const res of input) {
+    console.log(res);
+  }
+  return answer;
+}
+
+const question = [
+  [3, 4, 1, 2],
+  [4, 3, 2, 1],
+  [3, 1, 4, 2],
+];
+console.log(solution(question));
