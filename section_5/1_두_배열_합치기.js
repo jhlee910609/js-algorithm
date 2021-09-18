bebb8a520ffb9e021b533eda0d960a50100125e3
JunHee Lee <jhlee910609@gmail.com>
2021-09-19 00:00:34 +0900

    September 19, 2021 12:00 AM

--- section_5/1_두_배열_합치기.js
+++ section_5/1_두_배열_합치기.js
@@ -2,6 +2,12 @@
 // 135
 // 5
 // 23679
-function solution(a, b) {}
+function solution(a, b) {
+  let i = (j = 0);
+  const answer = [];
+  while (a.length > 0 || b.length > 0) {}
+
+  return answer;
+}
 
 console.log(solution([1, 3, 5], [2, 3, 6, 7, 9]));
