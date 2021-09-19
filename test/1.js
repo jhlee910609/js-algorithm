4da531456e8b404c11624ea36470c5602f2ff3e5
JunHee Lee <jhlee910609@gmail.com>
2021-09-19 23:55:53 +0900

    September 19, 2021 11:55 PM

--- test/1.js
+++ test/1.js
@@ -1,6 +1,14 @@
-function solution(a) {}
+function solution(a) {
+  let answer = "";
+  const temp = String(a).split("").reverse();
+  for (let i = 0; i < temp.length; i++) {
+    if (i && i % 3 === 0) answer += ",";
+    answer += temp[i];
+  }
+  return answer.reverse().join("");
+}
 
 // 123123555
 // 123,123,555
 
-console.log(solution(123123555));
+console.log(solution(1231238099555));
