6f518870b7eea302a2f740b406b9aa2aeb16713d
JunHee Lee <jhlee910609@gmail.com>
2021-09-20 10:09:25 +0900

    September 20, 2021 10:09 AM

---
+++ section_5/4_연속_부분수열_2.js
@@ -0,0 +1,13 @@
+function solution(input, M) {
+  const len = input.lenght;
+  let answer = 0;
+  const sum = 0;
+  const start = 0;
+
+  for (let rt = 0; rt < len; rt++) {
+    while (sum > M) {}
+  }
+  return answer;
+}
+
+console.log(solution());
