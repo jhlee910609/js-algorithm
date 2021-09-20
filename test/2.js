32e80c4d9e894deb79ea8f64d20aded5bb7ab8f9
JunHee Lee <jhlee910609@gmail.com>
2021-09-20 13:36:45 +0900

    September 20, 2021 1:36 PM

--- test/2.js
+++ test/2.js
@@ -1,6 +1,16 @@
 function solution(input) {
   const answer = [];
-  console.log(input);
+  for (let i = 0; i < input.length; i++) {
+    let temp = Math.floor(input[i] / 10) * 10;
+
+    if (input[i] % 10 > 5) {
+      temp += 10;
+    } else {
+      temp += 5;
+    }
+
+    console.log(temp - input[i]);
+  }
   return answer;
 }
 
