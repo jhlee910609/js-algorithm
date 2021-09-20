e19a307799e96d53df2bd9c9ff1b28a2c2570766
JunHee Lee <jhlee910609@gmail.com>
2021-09-20 10:33:00 +0900

    September 20, 2021 10:33 AM

--- test/숫자_문자열_영단어.js
+++ test/숫자_문자열_영단어.js
@@ -1,16 +1,24 @@
-// https://programmers.co.kr/learn/courses/30/lessons/81301
-function solution(input) {
-  let res = input;
-  res = res.replace("one", "1");
-  res = res.replace("two", "2");
-  res = res.replace("three", "3");
-  res = res.replace("four", "4");
-  res = res.replace("five", "5");
-  res = res.replace("six", "6");
-  res = res.replace("seven", "7");
-  res = res.replace("eight", "8");
-  res = res.replace("nine", "9");
-  return res;
+function solution(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
 }
 
-console.log(solution("one4seveneight") == 1478);
+console.log(solution("1two3zero"));
