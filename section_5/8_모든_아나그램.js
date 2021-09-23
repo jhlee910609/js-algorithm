5f52556e6e784d1b9df4d9b04328126686df0c0c
JunHee Lee <jhlee910609@gmail.com>
2021-09-24 00:12:21 +0900

    September 24, 2021 12:12 AM

--- section_5/8_모든_아나그램.js
+++ section_5/8_모든_아나그램.js
@@ -5,10 +5,15 @@ function solution(S, T) {
     map.set(t, (map.get(t) || 0) + 1);
   }
 
+  let cnt = 0;
   for (let i = 0; i < S.length - T.length + 1; i++) {
     const temp = S.slice(i, i + T.length);
+
     console.log(temp);
+    for (let a of temp) {
+    }
   }
+  return cnt;
 }
 
 console.log(solution("bacaAacba", "abc") === 3);
