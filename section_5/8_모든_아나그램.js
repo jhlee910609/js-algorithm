404cd86152f21cdaa834d8699bcef50eb2847bac
JunHee Lee <jhlee910609@gmail.com>
2021-09-24 00:40:42 +0900

    September 24, 2021 12:40 AM

--- section_5/8_모든_아나그램.js
+++ section_5/8_모든_아나그램.js
@@ -5,24 +5,35 @@ function solution(S, T) {
   const sMap = new Map();
 
   const compareMap = (aMap, bMap) => {
-      if(aMap.size !== bMap) return false;
-
-  }
+    if (aMap.size !== bMap) return false;
+  };
 
   // 1. tMap을 만든다.
   for (let t of T) {
     tMap.set(t, (tMap.get(t) || 0) + 1);
   }
 
+  // 2. length 할당
   const len = T.length - 1;
+
   for (let i = 0; i < len; i++) {
     sMap.set(S[i], (sMap.get(S[i]) || 0) + 1);
   }
 
   let lt = 0;
+
   for (let rt = len; i < S.length; i++) {
     sMap.set(S[rt], (sMap.get(S[rt]) || 0) + 1);
-    if(compareMap()
+    if (compareMap(sMap, tMap)) {
+      answer++;
+    }
+    const temp = sMap.get(S[lt]);
+    if (!temp) {
+      sMap.delete(S[lt]);
+    } else {
+      sMap.set(S[lt], temp - 1);
+    }
+    lt++;
   }
   return answer;
 }
