332331f2dfe7e859b49ee6c4950caf42e8d2bb99
JunHee Lee <jhlee910609@gmail.com>
2021-09-23 22:24:15 +0900

    September 23, 2021 10:24 PM

--- section_5/6_학급회장선출.js
+++ section_5/6_학급회장선출.js
@@ -1,3 +1,13 @@
-function solution(input) {}
+function solution(input) {
+  const map = new Map();
+  input.split("").forEach((ele) => {
+    const prev = map.get(ele) || 0;
+    map.set(ele, prev + 1);
+  });
+  let answer;
+  for (const [k, v] of map) {
+    console.log(k, v);
+  }
+}
 
 console.log(solution("BACBACCACCBDEDE"));
