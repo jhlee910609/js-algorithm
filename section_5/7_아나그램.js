d26d277ead4baffec3c1a277a2946a536a5da0f8
JunHee Lee <jhlee910609@gmail.com>
2021-09-23 22:45:22 +0900

    September 23, 2021 10:45 PM

--- section_5/7_아나그램.js
+++ section_5/7_아나그램.js
@@ -1,8 +1,22 @@
 function solution(a, b) {
-  let answer = "NO";
+  const aMap = new Map();
+  const bMap = new Map();
 
-  return answer;
+  for (let i = 0; i < a.length; i++) {
+    const aTemp = aMap.get(a[i]) || 0;
+    aMap.set(a[i], aTemp + 1);
+
+    const bTemp = bMap.get(b[i]) || 0;
+    bMap.set(b[i], bTemp + 1);
+  }
+
+  for (let [k, v] of aMap) {
+    if (v !== bMap.get(k)) {
+      return "NO";
+    }
+  }
+  return "YES";
 }
 
-console.log(solutions("AbaAeCe", "baeeACA") === "YES");
-console.log(solutions("abaCC", "Caaab") === "NO");
+console.log(solution("AbaAeCe", "baeeACA") === "YES");
+console.log(solution("abaCC", "Caaab") === "NO");
