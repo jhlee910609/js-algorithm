b198b547a3ede8343ee1785b63664989acabd07a
JunHee Lee <jhlee910609@gmail.com>
2021-09-28 00:03:47 +0900

    September 28, 2021 12:03 AM

--- section_6/3_크레인_인형뽑기.js
+++ section_6/3_크레인_인형뽑기.js
@@ -1,22 +1,29 @@
 function solution(board, moves) {
   let answer = 0; // 개수
-  const len = board.length;
   const stack = []; // 스택
 
   // 크레인 움직인다.
-  loop: for (const x of moves) {
-    // 보드 계속 돈다
-    console.log("x ===", x);
-    for (let i = 0; i < len; i++) {
-      const doll = board[i][x - 1];
+  // x는 target position
+  for (const x of moves) {
+    // 보드를  계속 돈다
+    for (let i = 0; i < board.length; i++) {
+      const doll = board[i][x - 1]; // 인형
+
+      // 인형이 있을때,
       if (doll !== 0) {
-        stack.push(doll);
+        // 스택 마지막 인형과 같으면
+        if (doll === stack[stack.length - 1]) {
+          stack.pop(); // 마지막 것도 없애고
+          answer += 2; // +2
+        } else {
+          // 아니면 push
+          stack.push(doll);
+        }
         board[i][x - 1] = 0;
-        continue loop;
+        break; // 포문 멈춤
       }
     }
   }
-  console.log(stack);
   return answer;
 }
 
