f639f3db9705ac3aa23fd11f8820be77f3370906
JunHee Lee <jhlee910609@gmail.com>
2021-09-30 00:51:03 +0900

    September 30, 2021 12:51 AM

--- section_6/4_후위식_연산.js
+++ section_6/4_후위식_연산.js
@@ -3,30 +3,32 @@ function solution(input) {
   let res;
   for (const x of input) {
     if (!isNaN(x)) {
-      stack.push(x);
+      stack.push(x / 1);
     } else {
       let temp;
+      const second = stack.pop();
+      const first = stack.pop();
+
       if (x === "+") {
-        temp = stack.pop() + stack.pop();
+        temp = first + second;
       }
 
       if (x === "-") {
-        temp = stack.pop() - stack.pop();
+        temp = first - second;
       }
 
       if (x === "*") {
-        temp = stack.pop() * stack.pop();
+        temp = first * second;
       }
 
       if (x === "/") {
-        temp = stack.pop() / stack.pop();
+        temp = first / second;
       }
+
       stack.push(temp);
     }
   }
-
-  console.log(stack);
-  return res;
+  return stack[0];
 }
 
 console.log(solution("352+*9-") === 12);
