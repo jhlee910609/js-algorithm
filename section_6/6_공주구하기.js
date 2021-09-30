c41c178e9e7e589b23e5352073b5cb9c3ab40379
JunHee Lee <jhlee910609@gmail.com>
2021-10-01 01:54:37 +0900

    October 1, 2021 1:54 AM

--- section_6/6_공주구하기.js
+++ section_6/6_공주구하기.js
@@ -1,3 +1,10 @@
-function solution(input) {}
+function solution(N, K) {
+  const q = [];
+  for (let i = 1; i <= N; i++) {
+    q.push(i);
+  }
+
+  while (q.length !== 1) {}
+}
 
 console.log(solution(8, 3));
