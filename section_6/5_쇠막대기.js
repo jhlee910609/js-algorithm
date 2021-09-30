ed21455cda8bcd7b1b44d6c47a187c8d4362b64f
JunHee Lee <jhlee910609@gmail.com>
2021-10-01 01:32:01 +0900

    October 1, 2021 1:32 AM

--- section_6/5_쇠막대기.js
+++ section_6/5_쇠막대기.js
@@ -1,12 +1,6 @@
 function solution(input) {
   let ans = 0;
   const stack = [];
-  for (const x of input) {
-    if (x === ")") {
-    } else {
-      stack.push(x);
-    }
-  }
 
   return ans;
 }
