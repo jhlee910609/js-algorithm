667d96f93b49cc3b440030e836849ceba70400ba
JunHee Lee <jhlee910609@gmail.com>
2021-10-02 23:41:11 +0900

    October 2, 2021 11:41 PM

--- section_6/7_교육과정_설계.js
+++ section_6/7_교육과정_설계.js
@@ -1,3 +1,13 @@
-function solution(input) {}
+function solution(a, b) {
+  const temp = [...a];
 
-console.log(solution());
+  let lastIdx = -1;
+  let ans = "YES";
+  let cur = temp.shift();
+
+  for (let i = 0; i < b.length; i++) {}
+
+  return ans;
+}
+
+console.log(solution("CBA", "CADBGE") === "YES");
