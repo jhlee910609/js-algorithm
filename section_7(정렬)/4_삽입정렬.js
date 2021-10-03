8ce9bc17ba915856c353efc677f20d1e48a12b02
JunHee Lee <jhlee910609@gmail.com>
2021-10-03 19:03:15 +0900

    October 3, 2021 7:03 PM

--- section_7(정렬)/4_삽입정렬.js
+++ section_7(정렬)/4_삽입정렬.js
@@ -1,16 +1,18 @@
 function solution(input) {
-  for (let i = 0; i < input.length; i++) {
-    console.log("=====", input[i]);
+  for (let i = 1; i < input.length; i++) {
     let minIdx = i;
-    for (let j = i; j < input.length; j++) {
-      if (input[j] < input[i]) {
+    console.log("=====", input[i]);
+    for (let j = 0; j < i; j++) {
+      console.log(input[j]);
+      if (input[j] < input[minIdx]) {
         minIdx = j;
       }
     }
+    [input[i], input[minIdx]] = [input[minIdx], input[i]];
   }
   return input;
 }
 
 const result = [13, 5, 345, 5, 38, 854, 243, 1, 42, 67].sort((a, b) => a - b);
-console.log(result);
 console.log(solution([13, 5, 345, 5, 38, 854, 243, 1, 42, 67]));
+console.log(result);
