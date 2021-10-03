614a3ed74b9dec2b0f115a1e77f6befccf3c4aab
JunHee Lee <jhlee910609@gmail.com>
2021-10-03 17:15:20 +0900

    October 3, 2021 5:15 PM

---
+++ section_7(정렬)/1_선택정렬.js
@@ -0,0 +1,18 @@
+// 선택정렬로 풀기
+function solution(input) {
+  for (let i = 0; i < input.length; i++) {
+    let min = i;
+    for (let j = i; j < input.length; j++) {
+      if (input[min] > input[j]) min = j;
+    }
+
+    const temp = input[i];
+    input[i] = input[min];
+    input[min] = temp;
+  }
+  return input;
+}
+
+const result = [13, 5, 345, 5, 38, 854, 243, 1, 42, 67].sort((a, b) => a - b);
+console.log(result);
+console.log(solution([13, 5, 345, 5, 38, 854, 243, 1, 42, 67]));
