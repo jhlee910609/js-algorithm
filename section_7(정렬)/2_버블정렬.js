16f0b69c1d95b65d671b8fe72c973836182e7e4f
JunHee Lee <jhlee910609@gmail.com>
2021-10-03 17:36:53 +0900

    October 3, 2021 5:36 PM

---
+++ section_7(정렬)/2_버블정렬.js
@@ -0,0 +1,9 @@
+// 버플정렬로 sort 문제 푼다
+function solution(input) {
+  // 가장 첫번째는 돌 필요 없다.
+  for (let i = input.length - 1; i > 0; i--) {}
+}
+
+const result = [13, 5, 345, 5, 38, 854, 243, 1, 42, 67].sort((a, b) => a - b);
+console.log(result);
+console.log(solution([13, 5, 345, 5, 38, 854, 243, 1, 42, 67]));
