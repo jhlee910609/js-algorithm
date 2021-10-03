7bfc0f9b9c0b7ac603386d953c6ddb3be08961d6
JunHee Lee <jhlee910609@gmail.com>
2021-10-04 00:49:42 +0900

    October 4, 2021 12:49 AM

--- section_7(정렬)/5_LRU.js
+++ section_7(정렬)/5_LRU.js
@@ -1,4 +1,13 @@
-function solution(input) {}
+function solution(S, tasks) {
+  const cache = Array.from({ length: S });
+  console.log("cache", cache);
 
-console.log(solution([1, 2, 3, 2, 6, 2, 3, 5, 7]));
+  tasks.forEac((task, i) => {
+    console.log("task", task);
+    if (cache.includes(task)) {
+    }
+  });
+}
+
+console.log(solution(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
 console.log(7, 5, 3, 2, 6);
