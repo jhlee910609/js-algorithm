31dea00a802a1141b4d991061658f4c96bc71ef3
JunHee Lee <jhlee910609@gmail.com>
2021-10-03 17:50:31 +0900

    October 3, 2021 5:50 PM

--- section_7(정렬)/2_버블정렬.js
+++ section_7(정렬)/2_버블정렬.js
@@ -1,7 +1,15 @@
 // 버플정렬로 sort 문제 푼다
 function solution(input) {
   // 가장 첫번째는 돌 필요 없다.
-  for (let i = input.length - 1; i > 0; i--) {}
+  for (let i = input.length - 1; i > 0; i--) {
+    // i는 큰 수
+    for (let j = 0; j < i; j++) {
+      if (input[j] > input[i]) {
+        [input[i], input[j]] = [input[j], input[i]];
+      }
+    }
+  }
+  return input;
 }
 
 const result = [13, 5, 345, 5, 38, 854, 243, 1, 42, 67].sort((a, b) => a - b);
