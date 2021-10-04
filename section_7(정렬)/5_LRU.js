bba695364c7a22ce564e91fa2fac1cd4ee8bb5cb
JunHee Lee <jhlee910609@gmail.com>
2021-10-05 00:46:21 +0900

    October 5, 2021 12:46 AM

--- section_7(정렬)/5_LRU.js
+++ section_7(정렬)/5_LRU.js
@@ -1,7 +1,7 @@
 function solution(S, tasks) {
   const cache = Array.from({ length: S }).map((v, i) => 0);
   tasks.forEach((task) => {
-    const cachedIdx = cache.indexOf(task);
+    const cachedIdx = cache.indexOf(task); // hit or miss
     const len = cachedIdx > -1 ? cachedIdx : cache.length - 1;
 
     for (let i = len; i > 0; i--) {
@@ -12,5 +12,28 @@ function solution(S, tasks) {
   return cache;
 }
 
+// 내장함수 사용
+function solution2(S, tasks) {
+  const cache = Array.from({ length: S }).map((v, i) => 0);
+  tasks.forEach((task) => {
+    const cachedIdx = cache.indexOf(task); // hit or miss
+
+    // miss
+    if (cachedIdx < 0) {
+      cache.unshift(task);
+      if (cache.length > S) {
+        cache.pop();
+      }
+    } else {
+      // hit
+      // 기존 cache 되어 있는 원소 삭제
+      cache.splice(cachedIdx, 1);
+      cache.unshift(task);
+    }
+  });
+  return cache;
+}
+
 console.log(solution(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
+console.log(solution2(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
 console.log(7, 5, 3, 2, 6);
