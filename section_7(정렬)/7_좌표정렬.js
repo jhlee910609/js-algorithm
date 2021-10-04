e412d7efb3650b3a5cfaba0bd35dc6cee8e6e0e8
JunHee Lee <jhlee910609@gmail.com>
2021-10-05 01:30:34 +0900

    October 5, 2021 1:30 AM

--- section_7(정렬)/7_좌표정렬.js
+++ section_7(정렬)/7_좌표정렬.js
@@ -1,5 +1,23 @@
 function solution(input) {
-  return input.sort((a, b) => console(a, b));
+  return input.sort((a, b) => a[0] - b[0] + (a[1] - b[1]));
 }
 
-console.log(solution([[2, 7], [1, 3][(1, 2)], [2, 5], [3, 6]]));
+console.log(
+  solution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
+
+console.log(
+  solution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
