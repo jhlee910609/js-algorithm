395c65e2a05f05d2bb65a9d0424f09126d1b6797
JunHee Lee <jhlee910609@gmail.com>
2021-10-07 08:29:18 +0900

    October 7, 2021 8:29 AM

--- section_7(정렬)/8_회의실배정.js
+++ section_7(정렬)/8_회의실배정.js
@@ -1,3 +1,28 @@
-function solution(input) {}
+function solution(timeTable) {
+  let ans = 1;
+  timeTable.sort((a, b) => {
+    return a[1] - b[1];
+  });
 
-console.log(solution());
+  let cur = timeTable[0];
+  for (let i = 1; i < timeTable.length; i++) {
+    const next = timeTable[i];
+    if (cur[1] <= next[0]) {
+      ans++;
+      cur = next;
+    }
+  }
+
+  return ans;
+}
+
+console.log(
+  "ans:",
+  solution([
+    [1, 4],
+    [2, 3],
+    [3, 5],
+    [4, 6],
+    [5, 7],
+  ])
+);
