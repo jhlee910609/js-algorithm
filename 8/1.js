687bb5c9ce92db400295259287bfa060a34a2f3c
JunHee Lee <jhlee910609@gmail.com>
2021-10-07 08:40:23 +0900

    October 7, 2021 8:40 AM

--- 8/1.js
+++ 8/1.js
@@ -1,5 +1,13 @@
 function solution(num) {
-  if (num) console.log(num);
+  DFS(num);
+
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  }
 }
 
 console.log(solution(20));
