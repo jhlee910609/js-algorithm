48c2b658a50181cdcb357925670076d0df78bdce
JunHee Lee <jhlee910609@gmail.com>
2021-10-07 23:46:28 +0900

    October 7, 2021 11:46 PM

--- section_7(정렬)/9_결혼식.js
+++ section_7(정렬)/9_결혼식.js
@@ -1,6 +1,15 @@
 function solution(input) {
   let ans = 0;
 
+  input.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+
+  console.log(input);
+
   return ans;
 }
 
