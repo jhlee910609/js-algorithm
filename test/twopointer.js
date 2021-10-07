80d4e3fd551a814bb87036adc6c64fd132265cfc
JunHee Lee <jhlee910609@gmail.com>
2021-10-07 20:33:00 +0900

    October 7, 2021 8:33 PM

---
+++ test/twopointer.js
@@ -0,0 +1,14 @@
+function solution(input) {
+  let start = 0,
+    end = 0;
+
+  let ans = -1;
+  let cur = 0;
+  while (start < input.length) {
+    if (input[start] <= input[end]) {
+    }
+  }
+  return ans;
+}
+
+console.log(solution([1, 2, 3, 2, 5]));
