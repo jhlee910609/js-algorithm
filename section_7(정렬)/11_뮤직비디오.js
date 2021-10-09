c99c60d43511d46f7bef84c9f3f89a7d2c3b005d
JunHee Lee <jhlee910609@gmail.com>
2021-10-09 22:59:00 +0900

    October 9, 2021 10:59 PM

--- section_7(정렬)/11_뮤직비디오.js
+++ section_7(정렬)/11_뮤직비디오.js
@@ -1,8 +1,16 @@
+/**
+ * 이분검색 => 결정 알고리즘
+ */
 function solution(input, M) {
-  if (!input || !input.length) return 0;
+  let lt = Math.max(...input);
+  let rt = input.reduce((prev, cur) => prev + cur, 0);
 
-  const avg = input.reduce((prev, cur) => prev + cur, 0) / M;
-  console.log(avg);
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    let cnt = 0;
+    let temp = 0;
+    for (let i = 0; i < input.length; i++) {}
+  }
 }
 
 console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) === 17);
