77032c0e1f1e2ea8408e67f49ca664f3c5ebbc33
JunHee Lee <jhlee910609@gmail.com>
2021-10-09 23:57:11 +0900

    October 9, 2021 11:57 PM

--- section_7(정렬)/12_마구간.js
+++ section_7(정렬)/12_마구간.js
@@ -1,15 +1,39 @@
-function solution(input) {
+function solution(input, M) {
   input = input.sort((a, b) => a - b);
 
   function count(input, minDistance) {
-    let cnt = 1;
-    let endPoint = 0;
-    for (let i = 0; i < input.length; i++) {}
+    let cnt = 1; // 한 마리 놓고 시작
+    let endPoint = input[0];
+
+    for (let i = 1; i < input.length; i++) {
+      // 딱 떨어지지 않을 수 있음
+      if (input[i] - endPoint >= minDistance) {
+        endPoint = input[i];
+        cnt++;
+      }
+    }
+    return cnt;
   }
 
-  let lt = 1;
-  let rt = input[input.length - 1];
-  let distance = parseInt((lt + rt) / 2);
+  let lt = 1; // 거리
+  let rt = input[input.length - 1]; // 끝
+  let ans;
+
+  // 2분 검색 종료 조건
+  while (lt <= rt) {
+    let distance = parseInt((lt + rt) / 2); // mid 거리
+    if (count(input, distance) === M) {
+      ans = distance;
+      break;
+    } else {
+      if (count(input, distance) < M) {
+        rt = distance - 1;
+      } else {
+        lt = distance + 1;
+      }
+    }
+  }
+  return ans;
 }
 
 console.log(solution([1, 2, 8, 4, 9], 3));
