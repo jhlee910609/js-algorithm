26e90a26bb34ee75cc3e4f73b1fc3663dc480d66
JunHee Lee <jhlee910609@gmail.com>
2021-10-09 21:52:56 +0900

    October 9, 2021 9:52 PM

--- section_7(정렬)/10_이분검색.js
+++ section_7(정렬)/10_이분검색.js
@@ -1,12 +1,19 @@
+/**
+ * 이분탐색
+ * 1. 정렬되어 있어야 함
+ * 2. lt > rt && rt < lt 까지 무한 반복
+ */
 function solution(input, M) {
   input = input.sort((a, b) => a - b);
   let lt = 0;
   let rt = input.length - 1;
+  let ans;
 
   while (lt < rt && rt > lt) {
     const mid = parseInt((lt + rt) / 2);
     if (input[mid] === M) {
-      return mid;
+      ans = mid + 1; // 몇 번째 인지
+      break;
     } else {
       if (input[mid] > M) {
         rt = mid - 1;
@@ -15,6 +22,8 @@ function solution(input, M) {
       }
     }
   }
+
+  return ans;
 }
 
 console.log(solution([23, 87, 65, 12, 57, 32, 99, 81], 57));
