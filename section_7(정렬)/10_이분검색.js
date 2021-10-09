2fe8bd619a6b837e24e311070c4b327b224a9096
JunHee Lee <jhlee910609@gmail.com>
2021-10-09 21:40:28 +0900

    October 9, 2021 9:40 PM

--- section_7(정렬)/10_이분검색.js
+++ section_7(정렬)/10_이분검색.js
@@ -1,3 +1,10 @@
-function solution(input) {}
+function solution(input, M) {
+  let lt = 0;
+  let rt = input.length - 1;
+
+  while (lt <= input.length - 1 && rt >= 0) {
+    const mid = (lt + rt) / 2;
+  }
+}
 
 console.log(solution([23, 87, 65, 12, 57, 32, 99, 81]));
