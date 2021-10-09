72b80bad0685c5f2f11880e400f878926fb5d925
JunHee Lee <jhlee910609@gmail.com>
2021-10-09 23:46:58 +0900

    October 9, 2021 11:46 PM

--- section_7(정렬)/12_마구간.js
+++ section_7(정렬)/12_마구간.js
@@ -1,9 +1,11 @@
 function solution(input) {
   input = input.sort((a, b) => a - b);
+
+  function count(input, minDistance) {}
+
   let lt = 1;
   let rt = input[input.length - 1];
   let distance = parseInt((lt + rt) / 2);
-  console.log(distance);
 }
 
 console.log(solution([1, 2, 8, 4, 9], 3));
