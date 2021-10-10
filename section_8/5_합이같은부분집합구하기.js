0f3bccb10ce5bcfe5ab31557a54446a28ce28a2e
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 14:10:04 +0900

    October 10, 2021 2:10 PM

--- section_8/5_합이같은부분집합구하기.js
+++ section_8/5_합이같은부분집합구하기.js
@@ -1,7 +1,12 @@
 function solution(input) {
   let ans = false;
+  const total = input.reduce((prev, cur) => prev + cur, 0);
+
+  function dfs(L, sum) {}
+
+  dfs(input);
 
   return ans;
 }
 
-console.log(solution() === true);
+console.log(solution([1, 3, 5, 6, 7, 10]) === true);
