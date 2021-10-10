f62cb743b7457f4096412bac3c65522a9e19fe15
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 17:32:35 +0900

    October 10, 2021 5:32 PM

--- section_8/6_바둑이승차.js
+++ section_8/6_바둑이승차.js
@@ -1,10 +1,28 @@
 function solution(C, dogs) {
   const flags = Array.from({ length: dogs.length }).map((v) => false);
+  let max = -1;
 
   function dfs(L, sum) {
-    if (sum >= C) {
+    if (L === dogs.length) {
+      let temp = 0;
+      for (let i = 0; i < dogs.length; i += 1) {
+        if (flags[i]) {
+          temp += dogs[i];
+        }
+        if (temp <= C) {
+          max = Math.max(temp, max);
+        }
+      }
+    } else {
+      flags[L] = true;
+      dfs(L + 1, sum + flags[L]);
+
+      flags[L] = false;
+      dfs(L + 1, sum);
     }
   }
+  dfs(0, 0);
+  return max;
 }
 
-console.log(solution(259));
+console.log(solution(259, [81, 58, 42, 33, 61]));
