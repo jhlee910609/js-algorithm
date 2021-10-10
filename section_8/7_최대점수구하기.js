123926ffd78ade7635474137fffd2e32524ddba6
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 18:27:18 +0900

    October 10, 2021 6:27 PM

---
+++ section_8/7_최대점수구하기.js
@@ -0,0 +1,16 @@
+function solution(input) {
+  let ans = -1; // 최대 점수
+
+  return ans;
+}
+
+console.log(
+  solution([
+    [5, 20],
+    [10, 5],
+    [25, 12],
+    [15, 8],
+    [6, 3],
+    [7, 4],
+  ])
+);
