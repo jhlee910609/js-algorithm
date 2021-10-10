29c77982e9832ff8abf9e41319051b8664c30f9e
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 12:17:41 +0900

    October 10, 2021 12:17 PM

--- section_8/2.js
+++ section_8/2.js
@@ -1,8 +1,21 @@
 // 재귀로 이진수 출력하기
-function solution(input){
+function solution(input) {
+  let ans = "";
 
-    const binary
+  function binary(num) {
+    if (num < 2) {
+      ans += num;
+      return;
+    }
 
+    binary(parseInt(num / 2));
+    console.log(num);
+    ans += num % 2;
+  }
+
+  binary(input);
+  console.log("ans", ans);
+  return ans;
 }
 
-console.log(solution(11) == '10111');
+console.log(solution(11) == "1011");
