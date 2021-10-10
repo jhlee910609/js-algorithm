3ff1c681033039acf631c1fcc0cd46b63ef94a90
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 13:03:02 +0900

    October 10, 2021 1:03 PM

--- section_8/4_부분집합구하기.js
+++ section_8/4_부분집합구하기.js
@@ -1,6 +1,12 @@
 function solution(N) {
   const origin = Array.from({ length: N }).map((v, i) => i + 1);
-  function combination(arr) {}
+  function combination(arr) {
+    if (!arr.length) return;
+    else {
+      console.log(arr);
+      combination(arr.slice(1));
+    }
+  }
 
   combination(origin);
 }
