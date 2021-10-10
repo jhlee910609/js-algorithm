b6acdfe3d8bc328ebeff4536891976541b0879c5
JunHee Lee <jhlee910609@gmail.com>
2021-10-10 12:10:46 +0900

    October 10, 2021 12:10 PM

--- section_8/1.js
+++ section_8/1.js
@@ -1,13 +1,13 @@
 function solution(num) {
-  DFS(num);
-
   function DFS(L) {
     if (L === 0) return;
+    // 0이면 빠져 나온다.
     else {
-      DFS(L - 1);
-      console.log(L);
+      DFS(L - 1); // 재귀를 태우고
+      console.log(L); // 재귀가 끝나면 스택에 쌓인 로그를 출력한다.
     }
   }
+  DFS(num);
 }
 
-console.log(solution(20));
+solution(20);
