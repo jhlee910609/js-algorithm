5a83af22823853b11e66ead4a9ed7f50e107268a
JunHee Lee <jhlee910609@gmail.com>
2021-10-12 02:04:48 +0900

    October 12, 2021 2:04 AM

--- section_8/8_중복순열구하기.js
+++ section_8/8_중복순열구하기.js
@@ -4,12 +4,12 @@ function solution(n, m) {
   let answer = [];
   let tmp = Array.from({ length: m }, () => 0);
   function DFS(L) {
-    console.log(L, tmp);
     if (L === m) {
       answer.push(tmp.slice());
     } else {
       for (let i = 1; i <= n; i++) {
         tmp[L] = i; // tmp[0] = 1
+        console.log(L, tmp);
         DFS(L + 1); // tmp[1] = 1
         // tmp[2] = 1
       }
