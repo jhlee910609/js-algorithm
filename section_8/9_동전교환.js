cb77384bc7894d2dc08becc144f847021748524c
JunHee Lee <jhlee910609@gmail.com>
2021-10-17 23:43:43 +0900

    October 17, 2021 11:43 PM

--- section_8/9_동전교환.js
+++ section_8/9_동전교환.js
@@ -1,17 +1,21 @@
+// 최소 동전 개수
+
 function solution(N, M) {
   N.sort((a, b) => b - a);
   console.log(N);
   let ans;
 
-  //   function dfs(){
-  //       if(){
-  //           return
-  //       } else {
-
-  //       }
-
-  //   }
-
+  function dfs(total) {
+    if (total === 0) {
+      return;
+    } else {
+      const coin = N.shift();
+      ans += Math.floor(M / coin);
+      console.log(ans % coin);
+      dfs(ans % coin);
+    }
+  }
+  dfs(M);
   return ans;
 }
 
