4bfc25763ce8f3107d428f1de21f51fd269cf4e6
JunHee Lee <jhlee910609@gmail.com>
2021-10-17 23:40:30 +0900

    October 17, 2021 11:40 PM

--- section_8/9_동전교환.js
+++ section_8/9_동전교환.js
@@ -1,3 +1,18 @@
-function solution(N, M) {}
+function solution(N, M) {
+  N.sort((a, b) => b - a);
+  console.log(N);
+  let ans;
 
-console.log(solution(3, [1, 2, 5]));
+  //   function dfs(){
+  //       if(){
+  //           return
+  //       } else {
+
+  //       }
+
+  //   }
+
+  return ans;
+}
+
+console.log(solution([1, 2, 5], 15) === 15);
