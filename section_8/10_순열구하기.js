5ab494994bc582170f4d6822f81a4e632212c1ee
JunHee Lee <jhlee910609@gmail.com>
2021-10-18 00:28:11 +0900

    October 18, 2021 12:28 AM

--- section_8/10_순열구하기.js
+++ section_8/10_순열구하기.js
@@ -2,20 +2,20 @@ function solution(arr, M) {
   let ans = [];
 
   function dfs(L, res, temp) {
-    if (L === M) {
+    if (L === 1) {
       ans.push(res);
       return;
     } else {
       for (let i = 0; temp.length; i++) {
+          console.log("L", L, "res", res, "temp", [
+            ...temp.slice(0, i),
+            ...temp.slice(i + 1),
+          ]);
         dfs(
           L + 1,
           [...res, temp[i]],
           [...temp.slice(0, i), ...temp.slice(i + 1)]
         );
-        console.log("L", L, "res", res, "temp", [
-          ...temp.slice(0, i),
-          ...temp.slice(i + 1),
-        ]);
       }
     }
   }
