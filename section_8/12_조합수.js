0f239c23e72526e3244d74edd7d401c8d8b544b7
JunHee Lee <jhlee910609@gmail.com>
2021-10-19 00:25:16 +0900

    October 19, 2021 12:25 AM

--- section_8/12_조합수.js
+++ section_8/12_조합수.js
@@ -1,8 +1,12 @@
 function solution(n, r) {
+  // 1. 조건을 보고 미리 큰 배열을 만들어 둔다.
+  let dy = Array.from(Array(35), () => Array.from(35).fill(0));
   function dfs(_n, _r) {
+    // 2. 기존의 저장된 값이 있다면 그 값을 그냥 return 한다.
+    if (dy[_n][_r] > 0) return dy[_n][_r];
     if (_n === _r || _r === 0) return 1;
     else {
-      return dfs(_n - 1, _r - 1) + dfs(_n - 1, _r);
+      return (dy[_n][_r] = dfs(_n - 1, _r - 1) + dfs(_n - 1, _r));
     }
   }
   return dfs(n, r);
