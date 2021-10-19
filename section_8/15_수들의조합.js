6c11d85e3e3167a8affed3cafcd44ef2956822a6
JunHee Lee <jhlee910609@gmail.com>
2021-10-20 01:05:27 +0900

    October 20, 2021 1:05 AM

--- section_8/15_수들의조합.js
+++ section_8/15_수들의조합.js
@@ -2,25 +2,25 @@ function solution(arr, K, M) {
   if (!arr) return 0;
 
   let count = 0;
-  let ch = Array.from({ length: arr.length }).fill(0);
+  let ch = Array.from({ length: arr.length }).fill(false);
 
   function dfs(L, sum) {
     if (L === K) {
       if (sum % M === 0) {
         count++;
-      } else {
-        for (let i = 0; i < arr.length; i++) {
-          if (!ch[i]) {
-            dfs(L + 1, sum + arr[i]);
-            ch[i] = 1;
-          }
+      }
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (!ch[i]) {
+          ch[i] = 1;
+          dfs(L + 1, sum + arr[i]);
+          ch[i] = 0;
         }
       }
     }
   }
 
   dfs(0, 0);
-
   return count;
 }
 
