5af7092ac6caf388bbf11ba8802e0b4e69819a99
JunHee Lee <jhlee910609@gmail.com>
2021-10-23 15:17:53 +0900

    October 23, 2021 3:17 PM

--- section_9/4_미로탐색.js
+++ section_9/4_미로탐색.js
@@ -1,34 +1,47 @@
-function solution(arr) {
+function solution(board) {
   let ans = 0;
   // x 방향
   let dx = [-1, 0, 1, 0];
   // y 방향
-  let dy = [0, -1, 0, 1];
-  let ch = Array.from({ length: arr.length }, () =>
-    Array.from({ length: arr[0].length }, () => 0)
-  );
+  let dy = [0, 1, 0, -1];
+
+  const xLen = board.length - 1;
+  const yLen = board[0].length - 1;
 
   function dsf(x, y) {
-    if (x < 0 || y < 0) return;
-    if (x > arr.length - 1 || y > arr[0].length - 1) return;
-    if (arr[x][y] === 1) return;
-
-    if (x === arr.length - 1 && y === arr[0].length - 1) {
-      console.log(ch);
+    if (x === xLen && y === yLen) {
       ans++;
     } else {
       for (let i = 0; i < dx.length; i++) {
         // 왔던 길 되돌아 가지 않도록 체크한다.
-        if (ch[x][y] === 0) {
-          ch[x][y] = 1;
-          dsf(x + dx[i], y + dy[i]);
-          ch[x][y] = 0;
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+
+        // 1. 경계를 벗어 났을 때에 대한 예외처리
+        // 2. 벽에 대한 예외처리 (벽이 아닐 때)
+
+        if (
+          nx >= 0 &&
+          nx <= xLen &&
+          ny >= 0 &&
+          ny <= yLen &&
+          board[nx][ny] === 0
+        ) {
+          // * 별도의 visited 배열 필요 없다.
+          // 어차피 벽이 아닐 때만 아래 함수가 실행된다.
+          // 방문 표시 한다.
+          board[nx][ny] = 1;
+          dsf(nx, ny);
+          // 방문 표시 해제.
+          // 방문 표시 안하면 무한으로 제자리 뺑뺑 돌게 된다.
+          board[nx][ny] = 0;
         }
       }
     }
   }
 
   // 기본 시작 좌표로 돌린다.
+  board[0][0] = 1;
   dsf(0, 0);
   console.log(ans);
   return ans;
@@ -42,8 +55,8 @@ let arr = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 1, 1, 1, 1, 1, 0],
   [0, 0, 0, 1, 0, 0, 0],
-  [1, 1, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
   [1, 1, 0, 1, 1, 0, 0],
   [1, 0, 0, 0, 0, 0, 0],
 ];
