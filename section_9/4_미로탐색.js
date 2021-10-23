6b6a8606cf4836b36beecc0584a621d82e5a8fbf
JunHee Lee <jhlee910609@gmail.com>
2021-10-23 15:02:49 +0900

    October 23, 2021 3:02 PM

--- section_9/4_미로탐색.js
+++ section_9/4_미로탐색.js
@@ -4,25 +4,33 @@ function solution(arr) {
   let dx = [-1, 0, 1, 0];
   // y 방향
   let dy = [0, -1, 0, 1];
+  let ch = Array.from({ length: arr.length }, () =>
+    Array.from({ length: arr[0].length }, () => 0)
+  );
 
   function dsf(x, y) {
     if (x < 0 || y < 0) return;
+    if (x > arr.length - 1 || y > arr[0].length - 1) return;
     if (arr[x][y] === 1) return;
 
-    if (x === 6 && y === 6) {
+    if (x === arr.length - 1 && y === arr[0].length - 1) {
+      console.log(ch);
       ans++;
     } else {
       for (let i = 0; i < dx.length; i++) {
-        console.log("x", x, "y", y);
-        console.log("dx", dx[i], "dy", dy[i]);
-        console.log("dx + x", x + dx[i], "dy + y", y + dy[i]);
-        dsf(x + dx[i], y + dy[i]);
+        // 왔던 길 되돌아 가지 않도록 체크한다.
+        if (ch[x][y] === 0) {
+          ch[x][y] = 1;
+          dsf(x + dx[i], y + dy[i]);
+          ch[x][y] = 0;
+        }
       }
     }
   }
 
   // 기본 시작 좌표로 돌린다.
   dsf(0, 0);
+  console.log(ans);
   return ans;
 }
 
