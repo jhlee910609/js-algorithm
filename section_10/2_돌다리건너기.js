cada72c01665ddf0d29ab5001e07babcd60a62d3
JunHee Lee <jhlee910609@gmail.com>
2021-10-31 17:23:35 +0900

    October 31, 2021 5:23 PM

--- section_10/2_돌다리건너기.js
+++ section_10/2_돌다리건너기.js
@@ -1,5 +1,10 @@
 function solution(N) {
+  let ans = 0;
   console.log(N);
+  let temp = Array.from({ length: N + 1 }, () => 0);
+  console.log(temp);
+
+  return ans;
 }
 
-console.log(solution());
+console.log(solution(7));
