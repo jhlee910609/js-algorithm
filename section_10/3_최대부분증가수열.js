aca59d2eacab69fc1b1fe45128a435e093368666
JunHee Lee <jhlee910609@gmail.com>
2021-10-31 18:17:45 +0900

    October 31, 2021 6:17 PM

--- section_10/3_최대부분증가수열.js
+++ section_10/3_최대부분증가수열.js
@@ -2,12 +2,16 @@ function solution(input) {
   const temp = Array.from({ length: input.length }, () => 0);
 
   for (let i = 0; i < input.length; i++) {
+    let maxLength = 0;
     for (let j = 0; j < i; j++) {
-      console.log(temp);
+      if (input[j] < input[i]) {
+        maxLength = Math.max(temp[j], maxLength);
+      }
     }
+    temp[i] = maxLength + 1;
   }
 
-  console.log(temp);
+  return Math.max(...temp);
 }
 
 console.log(solution([5, 3, 7, 8, 6, 2, 9, 4]) === 4);
