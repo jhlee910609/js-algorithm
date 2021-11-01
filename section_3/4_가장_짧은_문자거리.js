7a6ea58ad86b80f781ca92636eeba5a300c2a927
JunHee Lee <jhlee910609@gmail.com>
2021-11-02 01:55:07 +0900

    November 2, 2021 1:55 AM

--- section_3/4_가장_짧은_문자거리.js
+++ section_3/4_가장_짧은_문자거리.js
@@ -1,4 +1,7 @@
 // todo 복숩하기
+// 앞으로 한번 돌고, 뒤로 한번 돌고
+// 어느 기준으로 앞/뒤로 iteration 돌아야할 것 같은 경우
+// 걍 앞으로 한번 구하고, 뒤로 오면서 한번 더 구함
 function solution(input, target) {
   const len = input.length;
   let answer = "";
