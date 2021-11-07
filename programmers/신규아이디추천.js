95f0cac7255264d726a40096a5ade5349c2b466d
JunHee Lee <jhlee910609@gmail.com>
2021-11-08 00:18:30 +0900

    November 8, 2021 12:18 AM

--- programmers/신규아이디추천.js
+++ programmers/신규아이디추천.js
@@ -3,7 +3,10 @@ function solution(new_id) {
   return new_id
     .toLowerCase()
     .replace(/[^a-z0-9-_.]/g, "")
-    .replace(/[.]{2,}/g, "");
+    .replace(/[.]{2,}/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .replace(/^$/g, "a")
+    .replace();
 }
 
 console.log(solutions("...!@BaT#*..y.abcdefghijklm"));
