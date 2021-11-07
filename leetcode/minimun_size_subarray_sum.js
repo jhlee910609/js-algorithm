5630573ef5f8d56a245b17e34310adfd3bb2f2f4
JunHee Lee <jhlee910609@gmail.com>
2021-11-07 22:10:59 +0900

    November 7, 2021 10:10 PM

--- leetcode/minimun_size_subarray_sum.js
+++ leetcode/minimun_size_subarray_sum.js
@@ -6,15 +6,23 @@
 var minSubArrayLen = function (target, nums) {
   // length 만큼 연속해서
   let l = 0,
-    r = 0, sum=0;
+    r = 0,
+    sum = 0;
   let ans = Number.MAX_SAFE_INTEGER;
   // 길이
-    while(r < nums.length){
-        sum +=
+  while (r < nums.length) {
+    console.log(l, r);
+    sum += nums[r];
+    console.log("sum:", sum);
+
+    while (sum >= target) {
+      ans = Math.min(ans, r - l + 1);
+      sum -= nums[l++];
     }
+    r++;
+  }
 
-
-  return ans;
+  return ans !== Number.MAX_SAFE_INTEGER ? ans : 0;
 };
 
 console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
