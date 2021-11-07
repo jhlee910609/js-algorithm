62859577106f6bba53bca38e81272140006fd09e
JunHee Lee <jhlee910609@gmail.com>
2021-11-08 00:47:53 +0900

    November 8, 2021 12:47 AM

--- programmers/폰켓몬.js
+++ programmers/폰켓몬.js
@@ -1,23 +1,8 @@
 function solution(input) {
   const len = input.length / 2;
-  const visited = Array.from({ length: 10001 }, () => false);
-  let ans = -1;
+  const set = new Set(input);
 
-  function dfs(L) {
-    if (L === len) {
-      ans = Math.max(ans, visited.filter((v) => v).length);
-    } else {
-      for (let i = 0; i < input.length; i++) {
-        if (!visited[input[i]]) {
-          visited[input[i]] = true;
-          dfs(L + 1);
-          visited[input[i]] = false;
-        }
-      }
-    }
-  }
-  dfs(0);
-  return ans;
+  return Array(...set).length < len ? Array(...set).length : len;
 }
 
-console.log(solution([3, 3, 3, 2, 2, 2]));
+console.log(solution([3, 3, 3, 2, 2, 4]));
