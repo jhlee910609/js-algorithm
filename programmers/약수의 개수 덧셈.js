a841b6b530d4a3ee4e9f7f27b2b2f42cb3846bcf
JunHee Lee <jhlee910609@gmail.com>
2021-11-08 00:58:11 +0900

    November 8, 2021 12:58 AM

---
+++ programmers/약수의 개수 덧셈.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+  var answer = 0;
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        count++;
+      }
+    }
+    if (count % 2 === 0) answer += i;
+    else answer -= i;
+  }
+  return answer;
+}
+
+console.log(solution(13, 17) === 43);
