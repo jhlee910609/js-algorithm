fc5fcc2d6709401b4b4da71070edc7c970a911f3
JunHee Lee <jhlee910609@gmail.com>
2022-12-12 20:48:46 +0900

    ps solved!

--- leetcode/2023/70.climbing_step.js
+++ leetcode/2023/70.climbing_step.js
@@ -5,10 +5,8 @@
 const cache = {}; // time limit 떠서 캐시처리
 var climbStairs = function (n) {
   if (n === 1 || n === 0) return 1;
-  if (cache[n]) {
-    return cache[n];
-  }
-  const result = climbStairs(n - 1) + climbStairs(n - 2);
-  cache[n] = result;
-  return result;
+  if (cache[n]) return cache[n]; // 캐시에 있으면 그대로 return
+  const result = climbStairs(n - 1) + climbStairs(n - 2); // 없으면 재귀 연산
+  cache[n] = result; // 연산 후, 캐시 저장
+  return result; // 가지수 return
 };
