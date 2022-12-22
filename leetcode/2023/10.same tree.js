29bc63b90c7bc65ca237b29a9863f0016cba4bda
JunHee Lee <jhlee910609@gmail.com>
2022-12-22 14:44:36 +0000

    Dec 22, 2022, 11:44 PM

--- leetcode/2023/10.same tree.js
+++ leetcode/2023/10.same tree.js
@@ -1,1 +1,32 @@
 // 10. Same tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/***
+ * 1. 내가 생각한 풀이는 dfs
+ */
+var isSameTree = function (p, q) {
+  const s1 = [p];
+  const s2 = [q];
+
+  const ll = null,
+    rr = null;
+
+  while (s1.length && s2.length) {
+    ll = s1.pop();
+    rr = s2.pop();
+
+    if (!ll && !rr) continue;
+    if (!ll || !rr) return false;
+    if (ll.val !== rr.val) return false;
+
+    s1.push(ll.left);
+    s1.push(ll.right);
+    s2.push(rr.left);
+    s2.push(rr.right);
+  }
+};
