04ba785a48bd03727660760332ee1681f8ea0433
JunHee Lee <jhlee910609@gmail.com>
2022-12-22 16:24:15 +0000

    Dec 23, 2022, 1:24 AM

--- sample/main.js
+++ sample/main.js
@@ -3,8 +3,8 @@ window.onload = () => {
   const DRAGGING_ID = "dragging";
   const dragEle = findById(DRAGGING_ID);
 
-  let start = { x: 0, y: 0, w: 0, h: 0 };
-  let end = { x: 0, y: 0, w: 0, h: 0 };
+  let mouse = { x: 0, y: 0 };
+  let lastMouse = { x: 0, y: 0 };
   let isDragging = false;
 
   function findById(id) {
@@ -24,45 +24,25 @@ window.onload = () => {
 
   app.addEventListener("mousedown", (e) => {
     isDragging = true;
-    start.x = e.clientX;
-    start.y = e.clientY;
-    setDragPosition(start);
+    lastMouse.x = e.clientX;
+    lastMouse.y = e.clientY;
   });
 
   app.addEventListener("mousemove", (e) => {
-    if (!isDragging) return false;
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
 
-    const diffX = e.clientX - start.x;
-    const diffY = e.clientY - start.y;
-
-    if (diffX < 0) {
-      const temp = start.x;
-      start.x = e.clientX;
-      end.x = temp;
-    } else {
-      end.x = e.clientX;
+    if (isDragging) {
+      setDragPosition({
+        x: lastMouse.x,
+        y: lastMouse.y,
+        w: mouse.x - lastMouse.x,
+        h: mouse.y - lastMouse.y,
+      });
     }
-
-    if (diffY < 0) {
-      const temp = start.y;
-      start.y = e.clientY;
-      end.y = temp;
-    } else {
-      end.y = e.clientY;
-    }
-
-    const abs = Math.abs;
-    setDragPosition({
-      ...start,
-      w: abs(end.x - start.x),
-      h: abs(end.y - start.y),
-    });
   });
 
   app.addEventListener("mouseup", (e) => {
     isDragging = false;
-    start = {};
-    end = {};
-    setDragPosition(start);
   });
 };
